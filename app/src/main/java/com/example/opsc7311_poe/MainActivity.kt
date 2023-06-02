9f224b4b5b31d42cb305ac672d6cdec435bf756d
Cale Govindasami <ST10083941@vcconnect.edu.za>
2023-06-02 19:22:43 +0200

    Added project form fragment.

--- app/src/main/java/com/example/opsc7311_poe/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.opsc7311_poe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
