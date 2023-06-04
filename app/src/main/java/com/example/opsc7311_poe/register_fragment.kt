13b7073c802d406554c3ac0f85a34020b1614c97
lab_services_student <thileshneechinnasamy@gmail.com>
2023-06-04 23:37:36 +0200

    added things

--- app/src/main/java/com/example/opsc7311_poe/register_fragment.kt
+++ app/src/main/java/com/example/opsc7311_poe/register_fragment.kt
@@ -1,19 +1,16 @@
 package com.example.opsc7311_poe
 
-import android.nfc.Tag
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,11 +19,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [login_fragment.newInstance] factory method to
+ * Use the [register_fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class login_fragment : Fragment() {
-
+class register_fragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -40,57 +36,40 @@ class login_fragment : Fragment() {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_login_fragment, container, false)
+       val view =inflater.inflate(R.layout.fragment_register_fragment, container, false)
 
-
-         //initialize firebase auth
-        auth= Firebase.auth
-
-
-
-        val etEmail= view.findViewById<EditText>(R.id.etEmail)
+        val etRegEmail= view.findViewById<EditText>(R.id.etRegEmail)
+        val etConfirmEmail= view.findViewById<EditText>(R.id.etConfirmEmail)
         val etPassword = view.findViewById<EditText>(R.id.etPassword)
-        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
-        btnLogin.setOnClickListener {
+        val etConfirmPassword = view.findViewById<EditText>(R.id.etConfirmPassword)
+        val btnRegister=view.findViewById<Button>(R.id.btnRegister)
+        btnRegister.setOnClickListener {
             (this)
 
         }
-
-
-
-
-
-
-
-
-
-         return view
+        return view
     }
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email.trim(), password.trim())
+            .addOnCompleteListener(activity?.let { it }) {
 
-    private fun signInUser(email: String, password: String){
-        auth.signInWithEmailAndPassword(email.trim(), password)
-            .addOnCompleteListener(activity?.let{it}){
-                    it ->
                 if (it.isSuccessful) {
-                    Log.d(TAG, "signInWithEmail:success")
+                    Log.d(TAG, "createUserWithEmail:success")
+
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", it.exception)
+                    Toast.makeText(this, "Sign in failed.", Toast.LENGTH_SHORT).show
 
                 }
-                else{
-                    Log.w(TAG,"signInWithEmail:failure",it.exception)
-                Toast.makeText(this,"Sign in failed.",Toast.LENGTH_SHORT).show
-
-                }
-
-
-
             }
-
     }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -98,12 +77,12 @@ class login_fragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment login_fragment.
+         * @return A new instance of fragment register_fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            login_fragment().apply {
+            register_fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
