6e038491990ab8446a3e574f026d210061604f32
lab_services_student <thileshneechinnasamy@gmail.com>
2023-06-04 18:28:55 +0200

    added things

---
+++ app/src/main/java/com/example/opsc7311_poe/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.opsc7311_poe.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
