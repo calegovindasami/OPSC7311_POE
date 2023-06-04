ef13de5fbb038e3d2b4afabcd179289baf60ca61
Preshen4 <preshen4@gmail.com>
2023-06-04 12:26:14 +0200

    View Projects
    Add the code to get the data from firebase and view it in a recycler viewer. NOTE a screen for this needs to be implemented to make sure the code works

---
+++ app/src/main/java/data/ProjectAdapter.kt
@@ -0,0 +1,42 @@
+package data
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.opsc7311_poe.R
+
+class ProjectAdapter(private val projectList : ArrayList<ProjectViewModel>) : RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectAdapter.ProjectViewHolder {
+       // val itemView = View.inflate(parent.context, R.layout.project_list_item, null)
+        return ProjectViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: ProjectAdapter.ProjectViewHolder, position: Int) {
+        val current = projectList[position]
+        holder.projectName.text = current.name
+        holder.projectDescription.text = current.description
+        holder.projectStartDate.text = current.startDate.toString()
+        holder.projectEndDate.text = current.endDate.toString()
+        holder.projectMinimumDailyHours.text = current.minimumDailyHours.toString()
+        holder.projectMaximumDailyHours.text = current.maximumDailyHours.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return projectList.size
+    }
+
+    class ProjectViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        // TODO: Need a screen to use this method
+        val projectName = itemView.findViewById<TextView>(R.id.projectName)
+        val projectDescription = itemView.findViewById<TextView>(R.id.projectDescription)
+        val projectStartDate = itemView.findViewById<TextView>(R.id.projectStartDate)
+        val projectEndDate = itemView.findViewById<TextView>(R.id.projectEndDate)
+        val projectMinimumDailyHours = itemView.findViewById<TextView>(R.id.projectMinimumDailyHours)
+        val projectMaximumDailyHours = itemView.findViewById<TextView>(R.id.projectMaximumDailyHours)
+    }
+}
+
+
