ce07e763287d9125b68d78a96f6a35597255b79c
Cale Govindasami <ST10083941@vcconnect.edu.za>
2023-06-06 12:58:40 +0200

    Updated task form

--- app/src/main/java/TaskForm/TaskForm.kt
+++ app/src/main/java/TaskForm/TaskForm.kt
@@ -5,14 +5,22 @@ import Data.TaskViewModel
 import android.app.TimePickerDialog
 import android.net.Uri
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.opsc7311_poe.R
+import com.google.android.material.slider.Slider
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_CLOCK
+import com.google.android.material.timepicker.TimeFormat
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -33,12 +41,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TaskForm : Fragment() {
-    lateinit var imgUri: Uri
+    lateinit var imgUri: String
     lateinit var startTime: Date
 
     private val contract = registerForActivityResult(ActivityResultContracts.GetContent()) {
 
-        val imgUri = it
+        imgUri = it.toString()
     }
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -58,22 +66,21 @@ class TaskForm : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_task_form, container, false)
+        var btnStartTime = view.findViewById<Button>(R.id.btnTaskStartTime)
 
-        var btnGetDate = view.findViewById<Button>(R.id.btnTaskStartDateInput)
-
-        btnGetDate.setOnClickListener() {
-            startTime = createTimePicker(btnGetDate)
+        btnStartTime.setOnClickListener() {
+            createTimePicker(btnStartTime)
         }
 
-        val btnUploadImage = view.findViewById<Button>(R.id.btnTaskUploadImageInput)
-        btnUploadImage.setOnClickListener() {
+
+
+        val btnUploadImg = view.findViewById<Button>(R.id.btnTaskUploadImg)
+        btnUploadImg.setOnClickListener() {
             showImageSelector()
         }
-
         val btnSubmit = view.findViewById<Button>(R.id.btnTaskSubmit)
-
         btnSubmit.setOnClickListener() {
-            uploadData(getFormData(view))
+            uploadData(getFormData(view), view)
         }
 
         return view
@@ -85,54 +92,53 @@ class TaskForm : Fragment() {
 
     private fun getFormData(view: View): TaskViewModel {
 
-        var name = view.findViewById<EditText>(R.id.edtTaskNameInput).text.toString()
-        var desc = view.findViewById<EditText>(R.id.edtTaskDescriptionInput).text.toString()
-        var numHours = Integer.parseInt(view.findViewById<EditText>(R.id.edtTaskNumHoursInput).text.toString())
+        var name = view.findViewById<TextInputEditText>(R.id.edtTaskNameInput).text.toString()
+        var desc = view.findViewById<TextInputEditText>(R.id.edtTaskDescriptionInput).text.toString()
+        var numHours = view.findViewById<Slider>(R.id.rangeTaskNumHours).value.toInt()
 
-        return TaskViewModel(name, desc, startTime, numHours, imgUri.toString())
+        return TaskViewModel(name, desc, startTime, numHours, imgUri)
     }
 
-    private fun createTimePicker(button: Button): Date {
-        val calendar = Calendar.getInstance()
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        val min = calendar.get(Calendar.MINUTE)
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        var setDate = Date()
-
-        val timePickerDialog = activity?.let {
-            TimePickerDialog(it, TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                var hour = hourOfDay
-                var dateTime = "$year-$month-${day}T$hourOfDay:$minute:00"
-                setDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(dateTime)
-
-            }, hour, min,false)
+    private fun createTimePicker(button: Button){
+        val isSystem24Hour = DateFormat.is24HourFormat(activity?.let{it})
+        val clockFormat = if(isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+        val picker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(clockFormat)
+                .setHour(12)
+                .setMinute(10)
+                .setTitleText("Select start time")
+                .setInputMode(INPUT_MODE_CLOCK)
+                .build()
+        picker.addOnPositiveButtonClickListener {
+            val hour = picker.hour
+            val minute = picker.minute
+            val cal = Calendar.getInstance()
+            val day = cal.get(Calendar.DAY_OF_MONTH)
+            val month = cal.get(Calendar.MONTH)
+            val year = cal.get(Calendar.YEAR)
+            val date = "$day/$month/${year}T$hour:$minute"
+            startTime = SimpleDateFormat("dd/MM/yyyy'T'HH:mm").parse(date)
+            button.text = startTime.toString()
         }
-
-        timePickerDialog!!.show()
-        return setDate
+        picker.show(parentFragmentManager, "tag")
     }
 
-    private fun uploadData(task: TaskViewModel) {
+    private fun uploadData(task: TaskViewModel, contextView: View) {
         val db = Firebase.firestore
         val docRef =  db.collection("users").document("st10083941").collection("projects").document("jV6Qj5X4Yat2lgHwYm2W")
         docRef.get().addOnSuccessListener { documentSnapshot ->
             val project = documentSnapshot.toObject<ProjectViewModel>()
             project!!.tasks!!.add(task)
             docRef.update("tasks", project!!.tasks!!).addOnSuccessListener {
-                val wow = "works"
+                Toast.makeText(activity?.let{it}, "Task has been added!", Toast.LENGTH_LONG)
             }
+                .addOnFailureListener() {
+                    Toast.makeText(activity?.let{it}, "There has been an error", Toast.LENGTH_LONG)
+                }
 
         }
     }
-
-    private fun uploadImage() {
-        val storage = Firebase.storage
-        val storageRef = storage.reference
-    }
-
-
 
 
     companion object {
