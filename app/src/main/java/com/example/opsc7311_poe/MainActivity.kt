df72e5c6a7e31e215e5809ddcc26d95000a3b87e
Cale Govindasami <ST10083941@vcconnect.edu.za>
2023-06-06 22:58:37 +0200

    Refactored

--- app/src/main/java/com/example/opsc7311_poe/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.opsc7311_poe
 
-import Authentication.LoginFragment
-import Authentication.RegisterFragment
-import ProjectForm.ProjectForm
-import TaskForm.TaskForm
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -13,11 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val projectForm = ProjectForm.newInstance()
-        val taskForm = TaskForm.newInstance()
-        val register = RegisterFragment.newInstance()
-        val login = LoginFragment.newInstance()
-        supportFragmentManager.beginTransaction().replace(R.id.main_container, login).commit()
+
 
     }
 }
