e2d530a7e022e4281a91a650ead4faa4e44161de
ST10083422 <ST10083422@vcconnect.edu.za>
2023-07-02 00:18:39 +0200

    Excel function code added. Needs to be tested

---
+++ app/src/main/java/Services/ExportService.kt
@@ -0,0 +1,98 @@
+package Services
+
+/*import android.content.Context
+import android.os.Environment
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.FileOutputStream
+import java.io.IOException
+
+class ExportService(private val context: Context) {
+
+    fun createFile(){
+        val workbook: Workbook = XSSFWorkbook()
+        val sheet: Sheet = workbook.createSheet("Sheet 1")
+
+        // Write data to cells
+        val row: Row = sheet.createRow(0)
+        val cell: Cell = row.createCell(0)
+        cell.setCellValue("Hello")
+        row.createCell(1).setCellValue("World")
+
+
+        val filePath = "${context.getExternalFilesDir(null)}/example.xlsx"
+
+        try {
+            FileOutputStream(filePath).use { fileOut ->
+                workbook.write(fileOut)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+
+    }
+
+}*/
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import data.TaskViewModel
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.FileOutputStream
+import java.io.IOException
+
+class ExportService(private val context: Context) {
+
+    fun createFile(tasks:MutableList<TaskViewModel>){
+        val workbook: Workbook = XSSFWorkbook()
+        val sheet: Sheet = workbook.createSheet("Sheet 1")
+
+        var r = 0
+
+        while (r < tasks.size){
+       tasks.forEach(){
+           t->
+           var c = 0
+
+           val row: Row = sheet.createRow(r)
+           val cell: Cell = row.createCell(c)
+
+           cell.setCellValue(t.name)
+           row.createCell(c+1).setCellValue(t.startTime)
+           row.createCell(c+2).setCellValue(t.numberOfHours.toString())
+
+       }
+            r++
+        }
+
+        // Write data to cells
+
+
+        val filePath = "${context.getExternalFilesDir(null)}/example.xlsx"
+
+        try {
+            FileOutputStream(filePath).use { fileOut ->
+                workbook.write(fileOut)
+            }
+
+            openExcelFile(filePath)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun openExcelFile(filePath: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setDataAndType(Uri.parse("file://$filePath"), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        }
+    }
+}
