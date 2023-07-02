8c5858263a81398375230d5c9b75ef11a54a60aa
ST10083422 <ST10083422@vcconnect.edu.za>
2023-07-02 19:39:09 +0200

    Excel Function Working

--- app/src/main/java/Services/ExportService.kt
+++ app/src/main/java/Services/ExportService.kt
@@ -1,54 +1,22 @@
 package Services
 
-/*import android.content.Context
-import android.os.Environment
-import org.apache.poi.ss.usermodel.*
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import java.io.FileOutputStream
-import java.io.IOException
-
-class ExportService(private val context: Context) {
-
-    fun createFile(){
-        val workbook: Workbook = XSSFWorkbook()
-        val sheet: Sheet = workbook.createSheet("Sheet 1")
-
-        // Write data to cells
-        val row: Row = sheet.createRow(0)
-        val cell: Cell = row.createCell(0)
-        cell.setCellValue("Hello")
-        row.createCell(1).setCellValue("World")
-
-
-        val filePath = "${context.getExternalFilesDir(null)}/example.xlsx"
-
-        try {
-            FileOutputStream(filePath).use { fileOut ->
-                workbook.write(fileOut)
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-
-    }
-
-}*/
-
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Environment
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.FileProvider
 import data.TaskViewModel
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
 class ExportService(private val context: Context) {
 
-    fun createFile(tasks:MutableList<TaskViewModel>){
+    fun createFile(tasks: MutableList<TaskViewModel>) {
 
         val workbook: Workbook = HSSFWorkbook()
         val sheet: Sheet = workbook.createSheet("Sheet 1")
@@ -56,47 +24,55 @@ class ExportService(private val context: Context) {
         var r = 0
 
 
-       tasks.forEach(){
-           t->
-           var c = 0
+        tasks.forEach() { t ->
+            var c = 0
 
-           val row: Row = sheet.createRow(r)
-           val cell: Cell = row.createCell(c)
+            val row: Row = sheet.createRow(r)
+            val cell: Cell = row.createCell(c)
 
-           cell.setCellValue(t.name)
-           row.createCell(c+1).setCellValue(t.startTime)
-           row.createCell(c+2).setCellValue(t.numberOfHours.toString())
+            cell.setCellValue(t.name)
+            row.createCell(c + 1).setCellValue(t.startTime)
+            row.createCell(c + 2).setCellValue(t.numberOfHours.toString())
 
-           r++
+            r++
 
-       }
+        }
 
-
-
+        val path2 = getDownloadPath(context) + "/Sheet 1.xlsx"
         // Write data to cells
 
 
-        val filePath = "${context.getExternalFilesDir(null)}/example.xlsx"
+        val filePath = path2!!
 
         try {
-            FileOutputStream(filePath).use { fileOut ->
-                workbook.write(fileOut)
-            }
+            val file = File(filePath)
+            val fileOutputStream = FileOutputStream(file)
+            workbook.write(fileOutputStream)
+            fileOutputStream.close()
 
-            openExcelFile(filePath)
         } catch (e: IOException) {
             e.printStackTrace()
+        } finally {
+            workbook.close()
         }
 
     }
 
-    private fun openExcelFile(filePath: String) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.setDataAndType(Uri.parse("file://$filePath"), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-        if (intent.resolveActivity(context.packageManager) != null) {
-            context.startActivity(intent)
+    private fun getDownloadPath(context: Context): String? {
+        val downloadFolder =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        if (downloadFolder != null) {
+            if (!downloadFolder.mkdirs() && !downloadFolder.exists()) {
+                return null
+            }
+            return downloadFolder.absolutePath
+        } else {
+            val filesDir = context.getExternalFilesDir(null)
+            if (filesDir != null) {
+                return filesDir.absolutePath
+            }
         }
+        return null
     }
+
 }
